b066b63682f5f0ce1f42fb3ec89f88d2a1a2c4f4
Kanishka Anand <kanishkaadz@gmail.com>
2024-08-02 19:41:47 +0530

    colors

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Wambi',
       home: const HomePage()
     );
   }
