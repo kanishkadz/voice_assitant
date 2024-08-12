76d8e415ce47fadff7e2f3d6dcbd12aa3867b7b7
Kanishka Anand <kanishkaadz@gmail.com>
2024-08-12 21:41:34 +0530

    Update openai_service.dart

--- lib/openai_service.dart
+++ lib/openai_service.dart
@@ -1,6 +1,38 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:voice_assistant/secrets.dart';
 class OpenAIService {
 
   Future<String> isArtPromptAPI(String prompt) async{
-    
+    try{
+      final res = await http.post(
+          Uri.parse('https://api.openai.com/v1/chat/completions'),
+          headers: {
+            'Content-Type' : 'application/json',
+            'Authorization' : 'Bearer $openAiAPIKey',
+          },
+        body: jsonEncode({
+          "model": "gpt-4o-mini",
+          "messages" : [
+            {
+            'role' : 'user',
+              'content' : 'Does this message want to generate an AI pic',
+            }
+          ]
+        })
+
+      );
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> chatGPTAPI(String prompt) async{
+    return 'ChatGPT';
+  }
+
+  Future<String> dallEAPI(String prompt) async{
+    return 'Dall-E';
   }
 }
