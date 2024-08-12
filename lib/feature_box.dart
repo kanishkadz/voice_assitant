d3a29b6b596ecfb22e9835dde79b06e0cf41c033
Kanishka Anand <kanishkaadz@gmail.com>
2024-08-12 21:43:56 +0530

    Update feature_box.dart

--- lib/feature_box.dart
+++ lib/feature_box.dart
@@ -1,42 +1,59 @@
+import 'package:allen/pallete.dart';
 import 'package:flutter/material.dart';
-import 'package:voice_assistant/pallete.dart';
-
 
 class FeatureBox extends StatelessWidget {
   final Color color;
   final String headerText;
   final String descriptionText;
-  const FeatureBox({super.key, required this.color, required this.headerText, required this.descriptionText});
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.headerText,
+    required this.descriptionText,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(
+      margin: const EdgeInsets.symmetric(
         horizontal: 35,
         vertical: 10,
       ),
       decoration: BoxDecoration(
         color: color,
-        borderRadius: const BorderRadius.all(Radius.circular(15)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20).copyWith(left: 15, right: 15),
+        padding: const EdgeInsets.symmetric(vertical: 20.0).copyWith(
+          left: 15,
+        ),
         child: Column(
           children: [
             Align(
               alignment: Alignment.centerLeft,
-              child: Text(headerText, style: TextStyle(
-                fontFamily: 'Sera Pro',
-                color: Pallete.blackColor,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),),
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
             const SizedBox(height: 3),
-            Text(descriptionText, style: TextStyle(
-              fontFamily: 'Sera Pro',
-              color: Pallete.blackColor,
-            ),)
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Text(
+                descriptionText,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.blackColor,
+                ),
+              ),
+            ),
           ],
         ),
       ),
